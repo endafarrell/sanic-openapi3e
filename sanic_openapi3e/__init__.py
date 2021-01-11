a8f6d5c5dd37834c294856e90ed949682b5fbc55
Enda Farrell <enda.farrell@here.com>
2021-01-11 16:33:36 +0100

    Bump the minor version number

--- sanic_openapi3e/__init__.py
+++ sanic_openapi3e/__init__.py
@@ -2,5 +2,5 @@ from . import doc
 from .openapi import blueprint as openapi_blueprint
 from .swagger import blueprint as swagger_blueprint
 
-__version__ = "0.6.5"
+__version__ = "0.7.0"
 __all__ = ["openapi_blueprint", "swagger_blueprint", "doc"]
